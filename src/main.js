4d7c906bb63eb9ccc5a325d828f694a1251516f5
SithikaH <sithikahimandith11883@gmail.com>
2024-06-16 08:45:15 +0530

    m

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ function navigateToPage() {
   window.location.href = 'sites/sanjana.html'; 
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("load", function() {
+      const loadingOverlay = document.getElementById('loading-overlay');
+      loadingOverlay.classList.add('hidden');
+      document.body.style.overflow = 'auto'; 
+  });
+});
+
 
   
