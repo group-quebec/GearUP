7f1b160560d6de4783be897d6c2228fa103bb328
SithikaH <sithikahimandith11883@gmail.com>
2024-06-21 00:52:33 +0530

    neew

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ function navigateToPage() {
   window.location.href = 'sites/select.html'; 
 }
 
+function navigateToPageLogging(){
+  window.location.href = 'sites/login.html';
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("load", function() {
       const loadingOverlay = document.getElementById('loading-overlay');
